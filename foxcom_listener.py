c5003d6ae07fc80462993525b8d82deb3ce66f00
CJ <131416165+cjschultz87@users.noreply.github.com>
2023-09-05 03:49:30 -0400

    Update foxcom_listener.py

--- foxcom_listener.py
+++ foxcom_listener.py
@@ -15,15 +15,17 @@ except:
     quit()
     
 alpha = ''
-alpha_packet = []
     
 while True:
     alpha = sierra.recv(65535)
+    index = 0
     for element in alpha:
-        alpha_byte = bytearray(alpha)[alpha.index(element)]
+        alpha_byte = bytearray(alpha)[index]
         alpha_binary = ""
         for bit in dtb(alpha_byte,8):
             alpha_binary += str(bit)
-        print alpha_binary + "\t\t" + str(alpha_byte) + "\t\t...\t\t" + element
+        print alpha_binary + "\t\t" + str(alpha_byte) + "\t\t...\t\t" + element + "\t\t" + str(index)
         
-    print '\n***\n'
+        index += 1
+        
+    print '***'
